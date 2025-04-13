f1f976bedc2e70e9ead572da993395de76d8712a
Adam Munro <adam.munro@orbitremit.com>
2025-04-13 17:42:58 +1200

    fix: Raise BuilderInference & inline suspend

--- src/main/kotlin/nz/adjmunro/knomadic/raise/RaiseScope.kt
+++ src/main/kotlin/nz/adjmunro/knomadic/raise/RaiseScope.kt
@@ -5,19 +5,18 @@ import nz.adjmunro.knomadic.KnomadicDsl
 import nz.adjmunro.knomadic.raise.RaiseScope.Companion.catch
 import nz.adjmunro.knomadic.raise.RaiseScope.Companion.fold
 import nz.adjmunro.knomadic.raise.RaiseScope.Companion.raise
-import nz.adjmunro.knomadic.raise.RaiseScope.Companion.withRaiseScope
+import nz.adjmunro.knomadic.raise.RaiseScope.Companion.default
 import nz.adjmunro.knomadic.raise.exception.RaiseCancellationException
 import nz.adjmunro.knomadic.raise.exception.RaiseScopeLeakedException
 import nz.adjmunro.knomadic.util.nonFatalOrThrow
 import nz.adjmunro.knomadic.util.rethrow
-import kotlin.contracts.InvocationKind
 import kotlin.contracts.InvocationKind.AT_MOST_ONCE
 import kotlin.contracts.contract
 
 /**
  * A scope for raising errors.
  *
- * Use [withRaiseScope] context runner to create a new [RaiseScope]. Within the scope,
+ * Use [default] context runner to create a new [RaiseScope]. Within the scope,
  * the [raise] method can be called to throw any [Error] as if it were a [Throwable] and
  * in doing so, short-circuit the scope execution.
  *
@@ -36,9 +35,9 @@ public sealed interface RaiseScope<in Error : Any> {
     public fun complete()
 
     /**
-     * Wraps [error] as a [Throwable] and throws it, short-circuiting the [RaiseScope] execution.
+     * Wraps [raised] as a [Throwable] and throws it, short-circuiting the [RaiseScope] execution.
      *
-     * @param error The error to raise, which can be any non-null type.
+     * @param raised The error to raise, which can be any non-null type.
      * @return [Nothing] This function always throws.
      * @throws RaiseCancellationException When [RaiseScope] is active.
      * @throws RaiseScopeLeakedException If [RaiseScope.complete] has already been called.
@@ -62,29 +61,6 @@ public sealed interface RaiseScope<in Error : Any> {
     }
 
     public companion object {
-        @KnomadicDsl
-        @PublishedApi
-        internal inline fun <Ok : Any, Error : Any> withRaiseScope(
-            @BuilderInference action: RaiseScope<Error>.() -> Ok,
-        ): Ok = with(receiver = DefaultRaise<Error>(), action)
-
-        /**
-         * Invokes [error], wraps it as a [Throwable] and then throws it,
-         * short-circuiting the [RaiseScope] execution.
-         *
-         * @param error The error to raise, which can be any non-null type.
-         * @return [Nothing] This function always throws.
-         * @throws RaiseCancellationException When [RaiseScope] is active.
-         * @throws RaiseScopeLeakedException If [RaiseScope.complete] has already been called.
-         * @see RaiseScope.raise
-         */
-        @KnomadicDsl
-        public suspend inline fun <Error : Any> RaiseScope<Error>.raise(
-            crossinline error: suspend () -> Error,
-        ): Nothing {
-            raised(error = error())
-        }
-
         /**
          * A context runner use inside the [RaiseScope] when you wish to catch an expected
          * (non-fatal) [Throwable] and convert it to an [Error] from your domain.
@@ -102,9 +78,9 @@ public sealed interface RaiseScope<in Error : Any> {
          * @throws Throwable if [catch] re-throws.
          */
         @KnomadicDsl
-        public suspend inline fun <Ok : Any, Error : Any> RaiseScope<Error>.catch(
-            @BuilderInference catch: (throwable: Throwable) -> Error = ::rethrow,
-            @BuilderInference crossinline block: suspend RaiseScope<Error>.() -> Ok,
+        public inline fun <Ok : Any, Error : Any> RaiseScope<Error>.catch(
+            catch: (throwable: Throwable) -> Error = ::rethrow,
+            @BuilderInference block: RaiseScope<Error>.() -> Ok,
         ): Ok {
             contract {
                 callsInPlace(catch, AT_MOST_ONCE)
@@ -119,35 +95,71 @@ public sealed interface RaiseScope<in Error : Any> {
         }
 
         @KnomadicDsl
+        public inline fun <Ok : Any, Error : Any> Companion.default(
+            @BuilderInference action: RaiseScope<Error>.() -> Ok,
+        ): Ok = with(receiver = DefaultRaise<Error>(), action)
+
+        /** Ensures that the given [condition] is true, otherwise [raises][raise] an [Error]. */
+        @KnomadicDsl
+        public inline fun <Error : Any> RaiseScope<Error>.ensure(
+            condition: Boolean,
+            raise: () -> Error,
+        ) {
+            contract { returns() implies condition }
+            if (condition) raise(raise)
+        }
+
+        /** Ensures that the given [value] is not `null`, otherwise [raises][raise] an [Error]. */
+        @KnomadicDsl
+        public inline fun <Ok : Any, Error : Any> RaiseScope<Error>.ensureNotNull(
+            value: Ok?,
+            raise: () -> Error,
+        ): Ok {
+            contract { returns() implies (value != null) }
+            return value ?: raise(raise)
+        }
+
+        @KnomadicDsl
         @Suppress("UNCHECKED_CAST")
-        public suspend inline fun <In : Any, Out : Any, Error : Any> fold(
-            @BuilderInference crossinline block: suspend (scope: RaiseScope<Error>) -> In,
-            @BuilderInference crossinline catch: (throwable: Throwable) -> Out = ::rethrow,
-            @BuilderInference crossinline recover: suspend (error: Error) -> Out,
-            @BuilderInference crossinline transform: suspend (value: In) -> Out,
+        public inline fun <In : Any, Out : Any, Error : Any> RaiseScope<Error>.fold(
+            block: (scope: RaiseScope<Error>) -> In,
+            catch: (throwable: Throwable) -> Out = ::rethrow,
+            recover: (error: Error) -> Out,
+            transform: (value: In) -> Out,
         ): Out {
             contract {
-                // TODO: This seems to be a bug in the Kotlin compiler, it should be AT_MOST_ONCE?
-                //   ...or my understanding of contracts is wrong.
-                callsInPlace(block, InvocationKind.UNKNOWN)
-                callsInPlace(catch, InvocationKind.UNKNOWN)
-                callsInPlace(recover, InvocationKind.UNKNOWN)
-                callsInPlace(transform, InvocationKind.UNKNOWN)
+                callsInPlace(block, AT_MOST_ONCE)
+                callsInPlace(catch, AT_MOST_ONCE)
+                callsInPlace(recover, AT_MOST_ONCE)
+                callsInPlace(transform, AT_MOST_ONCE)
             }
 
-            return withRaiseScope {
-                try {
-                    val result = block(this@withRaiseScope)
-                    complete()
-                    transform(result)
-                } catch (e: RaiseCancellationException) {
-                    complete()
-                    recover(e.error as Error)
-                } catch (e: Throwable) {
-                    complete()
-                    catch(e.nonFatalOrThrow())
-                }
+            return try {
+                val result = block(this@fold)
+                complete()
+                transform(result)
+            } catch (e: RaiseCancellationException) {
+                complete()
+                recover(e.error as Error)
+            } catch (e: Throwable) {
+                complete()
+                catch(e.nonFatalOrThrow())
             }
         }
+
+        /**
+         * Invokes [error], wraps it as a [Throwable] and then throws it,
+         * short-circuiting the [RaiseScope] execution.
+         *
+         * @param error The error to raise, which can be any non-null type.
+         * @return [Nothing] This function always throws.
+         * @throws RaiseCancellationException When [RaiseScope] is active.
+         * @throws RaiseScopeLeakedException If [RaiseScope.complete] has already been called.
+         * @see RaiseScope.raise
+         */
+        @KnomadicDsl
+        public inline fun <Error : Any> RaiseScope<Error>.raise(
+            error: () -> Error
+        ): Nothing = raised(error = error())
     }
 }
