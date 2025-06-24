018bd8ad67f3142fa1e4ae26cbbb5348617b8959
Adam Munro <adam.munro@orbitremit.com>
2025-06-24 21:57:32 +1200

    fix: missing dsl annotation

--- src/main/kotlin/nz/adjmunro/knomadic/outcome/Cast.kt
+++ src/main/kotlin/nz/adjmunro/knomadic/outcome/Cast.kt
@@ -12,5 +12,6 @@ public inline fun <Ok : Any> successOf(value: Ok): Outcome.Success<Ok> = Outcome
 @KnomadicDsl
 public inline fun <Error : Any> failureOf(error: Error): Outcome.Failure<Error> = Outcome.Failure(error)
 
+@KnomadicDsl
 public fun <T> T.wrapOutcome(): Outcome<T & Any, Throwable> =
     throwfold(::failureOf) { it.nullfold(::failureOf, ::successOf) }
