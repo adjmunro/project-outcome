a3df121389a0ab2b11c77be88a72800ed6d307e4
Adam Munro <adam.munro@orbitremit.com>
2025-07-27 19:17:08 +1200

    feat: add adjmunro-inline

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,20 @@
 rootProject.name = "knomadic"
 
+fun getProperty(gradlePropertiesName: String, systemEnvName: String, instructions: String): String {
+    val result = providers.gradleProperty(gradlePropertiesName).orNull
+        ?.removeSurrounding(delimiter = "\"")
+        ?: System.getenv(systemEnvName)
+
+    return requireNotNull(value = result) {
+        """
+        Required property not found! Expected:
+        - Local: `$gradlePropertiesName` in `~/.gradle/gradle.properties`
+        - CI: exported environment variable `$systemEnvName`
+        $instructions
+        """.trimIndent()
+    }
+}
+
 pluginManagement {
     /**
      * The pluginManagement.repositories block configures the repositories that Gradle uses
@@ -29,6 +44,28 @@ dependencyResolutionManagement {
     repositories {
         mavenCentral()
         google()
+
+        val githubActor = getProperty(
+            gradlePropertiesName = "gpr.user",
+            systemEnvName = "GITHUB_ACTOR",
+            instructions = " Please set it to your GitHub username.",
+        )
+
+        val githubToken = getProperty(
+            gradlePropertiesName = "gpr.token",
+            systemEnvName = "GITHUB_TOKEN",
+            instructions = "Please set it to a valid GitHub token with `read:packages` permission."
+        )
+
+        listOf("adjmunro/project-inline").forEach { repository ->
+            maven {
+                url = uri("https://maven.pkg.github.com/$repository")
+                credentials {
+                    username = githubActor
+                    password = githubToken
+                }
+            }
+        }
     }
 
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
